62732c954fbf91d94fcc43956c29895ee4483b44
aaronben41 Github <aaronben41@gmail.com>
2022-10-31 12:21:02 +0100

    New commit

--- app/main.py
+++ app/main.py
@@ -2,31 +2,29 @@ import json
 from flask import Flask
 from flask_cors import CORS
 
-def create_app(test_config=None):
-     app = Flask(__name__)
-     # Set up CORS. Allow '*' for origins.
-     CORS(app, resources={r"/api/*": {"origins": "*"}})
- 
+app = Flask(__name__)
+# Set up CORS. Allow '*' for origins.
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 #Access-Control-Allow
 # CORS Headers
-     @app.after_request
-     def after_request(response):
-          response.headers.add(
-               "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
-          )
-          response.headers.add(
-               "Access-Control-Allow-Methods", "GET"
-          )
-          return response
+@app.after_request
+def after_request(response):
+     response.headers.add(
+          "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+     )
+     response.headers.add(
+          "Access-Control-Allow-Methods", "GET"
+     )
+     return response
 
 
-     @app.route('/', methods = ["GET"])
-     def index():
-               output = {
-                    "slackUsername": "aaronben41", 
-                    "backend": True, 
-                    "age": 25, 
-                    "bio": "I'm a self-motivated software developer with a flare for backend development" 
-               }
-               return json.dumps(output)
-     return app
+@app.route('/', methods = ["GET"])
+def index():
+     output = {
+          "slackUsername": "aaronben41", 
+          "backend": True, 
+          "age": 25, 
+          "bio": "I'm a self-motivated software developer with a flare for backend development. I love taking on new challenges" 
+     }
+     return json.dumps(output)

8bd134bb96d11ea261bb453f99d17c645a4b1fb5
aaronben41 Github <aaronben41@gmail.com>
2022-10-31 12:09:26 +0100

    updated CORS Header

--- app/main.py
+++ app/main.py
@@ -1,15 +1,34 @@
 import json
 from flask import Flask
+from flask_cors import CORS
+
+def create_app(test_config=None):
+     app = Flask(__name__)
+     CORS(app)
+
+# Set up CORS. Allow '*' for origins.
+     CORS(app, resources={r"/api/*": {"origins": "*"}})
+ 
+#Access-Control-Allow
+# CORS Headers
+     @app.after_request
+     def after_request(response):
+          response.headers.add(
+               "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+          )
+          response.headers.add(
+               "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+          )
+          return response
 
 
-app = Flask(__name__)
-
-@app.route('/', methods = ["GET"])
-def index():
-    output = {
-         "slackUsername": "aaronben41", 
-         "backend": True, 
-         "age": 25, 
-         "bio": "I'm a self-motivated software developer with a flare for backend development" 
-    }
-    return json.dumps(output)
+     @app.route('/', methods = ["GET"])
+     def index():
+               output = {
+                    "slackUsername": "aaronben41", 
+                    "backend": True, 
+                    "age": 25, 
+                    "bio": "I'm a self-motivated software developer with a flare for backend development" 
+               }
+               return json.dumps(output)
+     return app

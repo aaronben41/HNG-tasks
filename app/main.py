92f38fa6770a15d5c7949c6d627d7dbe9de99343
aaronben41 Github <aaronben41@gmail.com>
2022-10-31 12:43:02 +0100

    updated cors

--- app/main.py
+++ app/main.py
@@ -3,7 +3,18 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+   #Access-Control-Allow
+    # CORS Headers
+@app.after_request
+def after_request(response):
+     response.headers.add(
+          "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+     )
+     response.headers.add(
+          "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+     )
+     return response
 
 
 @app.route('/', methods = ["GET"])

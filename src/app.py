a67a7b4bb1642c3498c5ad15360e63f93ba49e56
aaronben41 Github <aaronben41@gmail.com>
2022-11-04 17:07:55 +0100

    Updated calculations

--- src/app.py
+++ src/app.py
@@ -1,73 +1,78 @@
 from flask import Flask, jsonify, request
-# from flask_cors import CORS
+from flask_cors import CORS
 import json, operator
 
 
 app = Flask(__name__)
 
-# CORS(app, resources={r"/api/*": {"origins": "*"}})
-#    #Access-Control-Allow
-#     # CORS Headers
-# @app.after_request
-# def after_request(response):
-#      response.headers.add(
-#           "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
-#      )
-#      response.headers.add(
-#           "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
-#      )
-#      return response
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+   #Access-Control-Allow
+    # CORS Headers
+@app.after_request
+def after_request(response):
+     response.headers.add(
+          "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+     )
+     response.headers.add(
+          "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+     )
+     return response
 
 
-ops = {
-    "+": operator.add,
-    "-": operator.sub,
-    "*": operator.mul,
-    "/": operator.truediv
-}
 
-ops_words = {
-    "+": 'addition',
-    "-": 'subtraction',
-    "*": 'multiplication',
-    "/": 'division'
-}
+
 
 #Stage 2 task
 
-@app.route('/', methods = ["GET"])
+@app.route('/', methods = ["POST"])
 def index():
      
      # default values
-     operation_type = '*'
+     operation_type = '-'
      x = 5
      y = 20
 
      def calculate ():
+          ops = {
+               "+": operator.add,
+               "-": operator.sub,
+               "*": operator.mul,
+               "/": operator.truediv
+               }
+
+          ops_words = {
+               "+": 'addition',
+               "-": 'subtraction',
+               "*": 'multiplication',
+               "/": 'division'
+               }
+               
           if operation_type in ops:
                operation = ops[operation_type]
+               operation_output = ops_words[operation_type]
                output = operation(x,y)
           else:
+               operation_output = "Error encountered"
                output = "please enter operator as a symbol, eg. '+' or '-' "
           
           result =  {
                "slackUsername": "aaronben41", 
-               "operation_type" : ops_words[operation_type],
+               "operation_type" : operation_output,
                "result": output 
                }
           return result
 
-     # body = request.get_json()
+     body = request.get_json()
 
-     # if body != None:
-     #      operation_type = body.get('operation_type')
-     #      x = int(body.get('x'))
-     #      y = int(body.get('y'))
+     if body != None:
+          operation_type = body['operation_type']
+          x = int(body['x'])
+          y = int(body['y'])
           
-     #      final_result = calculate()
+          final_result = calculate()
           
-     # else:
-     final_result = calculate()
+     else:
+          final_result = calculate()
 
      return json.dumps(final_result), 200, {'content-type':'application/json'}
      
